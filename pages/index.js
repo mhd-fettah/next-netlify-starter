f4755c4d4380e3e879ef05cc8d4b85242f2a6e04
Fettah <2024designlab@gmail.com>
2023-11-29 02:23:55 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         </p>
       </main>
 
-    <template>
+<template>
   <div class="feedback-form">
     <form
       netlify
@@ -42,39 +42,6 @@ export default function Home() {
     </form>
   </div>
 </template>
-
-<style scoped>
-.feedback-form {
-  margin: 40px 40px;
-  min-width: 250px;
-  width: 45%;
-}
-input,
-textarea,
-button {
-  font-size: inherit;
-  margin: 15px 0;
-  padding: 12px 20px;
-  width: 100%;
-}
-
-button {
-  background-color: teal;
-  border: 1px solid black;
-  color: white;
-  width: 50%;
-}
-
-.hidden {
-  clip: rect(0 0 0 0);
-  clip-path: inset(50%);
-  height: 1px;
-  overflow: hidden;
-  position: absolute;
-  white-space: nowrap;
-  width: 1px;
-}
-</style>
 
       <Footer />
     </div>
